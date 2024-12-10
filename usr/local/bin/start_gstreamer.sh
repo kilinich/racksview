0c284b71af817a2f0c5fb6e2060463849b1a17a7
Kirill Ilinich <kilinich@gmail.com>
2024-12-10 20:37:49 +0100

    add seconds

--- usr/local/bin/start_gstreamer.sh
+++ usr/local/bin/start_gstreamer.sh
@@ -6,7 +6,7 @@ gst-launch-1.0 libcamerasrc ! \
 video/x-raw,width=1024,height=768,framerate=1/1 ! \
 videoconvert ! \
 textoverlay text="$HOSTNAME" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
-clockoverlay time-format="%d-%m-%Y %H:%M" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
+clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
 videoconvert ! jpegenc quality=50 ! multipartmux ! queue ! \
 tcpserversink host=0.0.0.0 port=8080 \
 2>/var/log/gstreamer-error.log
