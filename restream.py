a546cc214fa04559af5eb95319efa47a0c9bc38d
Kirill Ilinich <kilinich@gmail.com>
2024-12-10 17:08:48 +0100

    Update restream.py

--- restream.py
+++ restream.py
@@ -5,24 +5,22 @@ app = Flask(__name__)
 
 TCP_SERVER_HOST = "127.0.0.1"
 TCP_SERVER_PORT = 8080
-BOUNDARY_STRING = "--ThisRandomString"  # Граница для MJPEG
+BOUNDARY_STRING = "--ThisRandomString"
 
 def generate_stream():
-    """Генератор потока MJPEG через сокетное подключение."""
     try:
         with socket.create_connection((TCP_SERVER_HOST, TCP_SERVER_PORT)) as sock:
             while True:
-                data = sock.recv(1024)  # Читаем данные порциями
-                if not data:  # Если поток закрыт
+                data = sock.recv(4096)  
+                if not data:
                     break
                 yield data
     except socket.error as e:
-        print(f"Ошибка подключения к {TCP_SERVER_HOST}:{TCP_SERVER_PORT}: {e}")
+        print(f"Error connecting to {TCP_SERVER_HOST}:{TCP_SERVER_PORT}: {e}")
         yield b''
 
 @app.route('/')
 def mjpeg_stream():
-    """Эндпоинт для трансляции MJPEG в браузер."""
     headers = {
         "Content-Type": f"multipart/x-mixed-replace; boundary={BOUNDARY_STRING}"
     }
