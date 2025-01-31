6a1031eecb6aada6fd74ee10b4755a272fdc7b13
Kirill Ilinich <kilinich@gmail.com>
2025-01-31 11:46:20 +0100

    Update start_gstreamer.sh

--- usr/local/bin/start_gstreamer.sh
+++ usr/local/bin/start_gstreamer.sh
@@ -4,14 +4,14 @@ export HOSTNAME=$(hostname)
 
 gst-launch-1.0 -e \
 libcamerasrc name=src ! queue ! video/x-raw,framerate=1/1 ! tee name=t \
-    t. ! queue ! videoscale ! video/x-raw,width=1600,height=1200 ! videoconvert ! \
+    t. ! queue ! videoscale ! video/x-raw,width=1240,height=768 ! videoconvert ! \
         textoverlay text="$HOSTNAME" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
         clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
         videoconvert ! jpegenc quality=60 ! multipartmux ! queue ! \
         tcpserversink host=0.0.0.0 port=8013 \
-    t. ! queue ! videoscale ! video/x-raw,width=640,height=480 ! videoconvert ! \
+    t. ! queue ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! \
         textoverlay text="$HOSTNAME" valignment=top halignment=left font-desc="Sans, 20" xpos=5 ypos=5 ! \
-        clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 20" xpos=5 ypos=-5 ! \
+        clockoverlay time-format="%H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 20" xpos=5 ypos=-5 ! \
         videoconvert ! jpegenc quality=80 ! multipartmux ! queue ! \
         tcpserversink host=0.0.0.0 port=8012 \
 2>/var/log/gstreamer.log
