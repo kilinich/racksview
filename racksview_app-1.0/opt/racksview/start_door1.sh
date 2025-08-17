0b4e834f4349b9bc7433371906ae36d025c80a8a
Kilinich <kilinich@gmail.com>
2025-08-17 18:05:07 +0200

    stream

--- racksview_app-1.0/opt/racksview/start_door1.sh
+++ racksview_app-1.0/opt/racksview/start_door1.sh
@@ -10,5 +10,4 @@ PIPE1="$DIR/door1"
 [[ -p "$PIPE1" ]] || mkfifo "$PIPE1"
 
 # Run the Python script and tee output to both pipes, ignore broken pipe errors
-python3 read_ultrasonic.py -d /dev/serial0 | tee  --output-error=exit-nopipe -p "$PIPE1" &
-cat "$PIPE1"
+python3 read_ultrasonic.py -d /dev/serial0 | tee  --output-error=exit-nopipe -p "$PIPE1" /var/log/racksview/door1.log
--- racksview_app-1.0/opt/racksview/start_gstreamer.sh
+++ racksview_app-1.0/opt/racksview/start_gstreamer.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
 export HOSTNAME=$(hostname)
-TARGET_BASE="/media/usb/video"
 
 # Start the gstreamer pipeline
-
-# This pipeline captures video from the camera, adds a text overlay with the hostname and current time, 
-# scales the video to two different resolutions, encodes the video as jpeg, and sends the video over TCP to two different ports.
-
-# The pipeline uses the libcamerasrc element to capture video from the camera. 
-
-# The tee element is used to split the video stream into two branches. 
-
-# The first branch scales the video to a resolution of 1296x972, adds a text overlay with the hostname and current time, 
-# encodes the video as jpeg with a quality of 60, and sends the video over TCP to port 8013. 
-
-# The second branch scales the video to a resolution of 320x240, adds a text overlay with the hostname and current time, 
-# encodes the video as jpeg with a quality of 80, and sends the video over TCP to port 8012.
-
 
 gst-launch-1.0 -e \
 libcamerasrc name=src ! queue ! video/x-raw,framerate=1/1 ! tee name=t \
     t. ! queue ! videoscale ! video/x-raw,width=1296,height=972 ! videoconvert ! \
         textoverlay text="$HOSTNAME" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
         clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
-        videoconvert ! jpegenc quality=70 ! multipartmux ! queue ! \
-        tcpserversink host=0.0.0.0 port=8013 \
+        videoconvert ! x264enc tune=zerolatency bitrate=2000 speed-preset=ultrafast ! \
+        h264parse ! mpegtsmux ! hlssink max-files=5 playlist-length=5 target-duration=5 \
+        location="/var/www/html/stream1/segment_%05d.ts" playlist-location="/var/www/html/stream1/playlist.m3u8" \
     t. ! queue ! videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! \
         textoverlay text="$HOSTNAME" valignment=top halignment=left font-desc="Sans, 20" xpos=5 ypos=5 ! \
         clockoverlay time-format="%H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 20" xpos=5 ypos=-5 ! \
-        videoconvert ! jpegenc quality=90 ! multipartmux ! queue ! \
-        tcpserversink host=0.0.0.0 port=8012 \
-    t. ! queue ! videorate ! video/x-raw,framerate=1/60 ! \
-        videoscale ! video/x-raw,width=320,height=240 ! videoconvert ! \
-        jpegenc quality=50 ! \
-        multifilesink location="${TARGET_BASE}/preview.jpg" post-messages=true next-file=0 max-file-duration=60000000000
+        videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=ultrafast ! \
+        h264parse ! mpegtsmux ! hlssink max-files=5 playlist-length=5 target-duration=5 \
+        location="/var/www/html/stream2/segment_%05d.ts" playlist-location="/var/www/html/stream2/playlist.m3u8"
