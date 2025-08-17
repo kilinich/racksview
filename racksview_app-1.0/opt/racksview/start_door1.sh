80b3f657e3f55b8c59a80e61495cf2e384bfe3ca
Kilinich <kilinich@gmail.com>
2025-08-17 16:37:04 +0200

    fix

--- racksview_app-1.0/opt/racksview/start_door1.sh
+++ racksview_app-1.0/opt/racksview/start_door1.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# This script starts the ultrasonic sensor reading process and sets up named pipes for communication.
 
 # Create /tmp/racksview directory if it doesn't exist
 DIR="/tmp/racksview"
 [[ -d "$DIR" ]] || mkdir -p "$DIR"
 
 # Create named pipes if they don't exist
-PIPE1="$DIR/racksview_door1"
-PIPE2="$DIR/racksview_sensor1"
+PIPE1="$DIR/door1"
 
 [[ -p "$PIPE1" ]] || mkfifo "$PIPE1"
-[[ -p "$PIPE2" ]] || mkfifo "$PIPE2"
 
 # Run the Python script and tee output to both pipes, ignore broken pipe errors
-python3 read_ultrasonic.py -d /dev/serial0 | tee  --output-error=exit-nopipe -p "$PIPE1" "$PIPE2"
+python3 read_ultrasonic.py -d /dev/serial0 | tee  --output-error=exit-nopipe -p "$PIPE1" &
+cat "$PIPE1"
