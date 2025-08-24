1045ed13ec9c99f39c6766a16a8de5cf531f8454
Kilinich <kilinich@gmail.com>
2025-08-24 18:25:34 +0200

    html

--- racksview_app-1.1/var/html/disable-top.js
+++ racksview_app-1.1/var/html/disable-top.js
@@ -1,7 +1,7 @@
 <script>
 document.addEventListener('DOMContentLoaded', function() {
     var header = document.querySelector('h1');
-    if (header && header.textContent.trim() === 'Index of /events/') {
+    if (header && header.textContent.trim() === 'Index of /video/') {
         var links = document.querySelectorAll('pre a');
         for (var i = 0; i < links.length; i++) {
             var linkText = links[i].textContent.trim();
