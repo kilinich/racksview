aaf45c216d7dbb89fac2de224588835bb6f2662b
Kilinich <kilinich@gmail.com>
2025-08-25 14:16:42 +0200

    final1

--- racksview_app-1.1/bin/start_gstreamer.sh
+++ racksview_app-1.1/bin/start_gstreamer.sh
@@ -44,12 +44,12 @@ if [[ "$CAM_DEV" == "CSI" ]]; then
     gst-launch-1.0 -q -e \
     libcamerasrc name=src ! queue leaky=2 ! video/x-raw,framerate=1/1 ! tee name=t \
         t. ! queue leaky=2 ! videoscale ! video/x-raw,width=1296,height=972 !\
-            textoverlay text="$OVLABEL" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
+            textoverlay text="$HOSTNAME-$OVLABEL" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
             clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
             v4l2jpegenc ! multipartmux ! \
             tcpserversink host=127.0.0.1 port=$PORT_HIGH recover-policy=3 sync=false \
         t. ! queue leaky=2 ! videoscale ! video/x-raw,width=320,height=240 ! \
-            textoverlay text="$OVLABEL" valignment=top halignment=left font-desc="Sans, 20" xpos=2 ypos=2 ! \
+            textoverlay text="$HOSTNAME-$OVLABEL" valignment=top halignment=left font-desc="Sans, 20" xpos=2 ypos=2 ! \
             clockoverlay time-format="%H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 20" xpos=2 ypos=-2 ! \
             jpegenc quality=60 ! multipartmux ! \
             tcpserversink host=127.0.0.1 port=$PORT_LOW recover-policy=3 sync=false
@@ -58,12 +58,12 @@ else
     gst-launch-1.0 -q -e \
     v4l2src device=$CAM_DEV ! queue leaky=2 ! image/jpeg,width=1280,height=800,framerate=10/1 ! videorate ! image/jpeg,framerate=1/1 ! v4l2jpegdec ! video/x-raw ! tee name=t \
         t. ! queue leaky=2 ! \
-            textoverlay text="$OVLABEL" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
+            textoverlay text="$HOSTNAME-$OVLABEL" valignment=top halignment=left font-desc="Sans, 10" xpos=10 ypos=10 ! \
             clockoverlay time-format="%d-%m-%Y %H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 10" xpos=10 ypos=-10 ! \
             v4l2jpegenc ! multipartmux ! \
             tcpserversink host=127.0.0.1 port=$PORT_HIGH recover-policy=3 sync=false \
         t. ! queue leaky=2 ! videoscale ! video/x-raw,width=320,height=240 ! \
-            textoverlay text="$OVLABEL" valignment=top halignment=left font-desc="Sans, 20" xpos=2 ypos=2 ! \
+            textoverlay text="$HOSTNAME-$OVLABEL" valignment=top halignment=left font-desc="Sans, 20" xpos=2 ypos=2 ! \
             clockoverlay time-format="%H:%M.%S" valignment=bottom halignment=left font-desc="Sans, 20" xpos=2 ypos=-2 ! \
             jpegenc quality=60 ! multipartmux ! \
             tcpserversink host=127.0.0.1 port=$PORT_LOW recover-policy=3 sync=false
