88c2bf67ba017c6ace6e65c1b3953581308671df
Kilinich <kilinich@gmail.com>
2025-08-25 19:52:51 +0200

    fix compile

--- racksview_app-1.1/src/motion_detector_compile.sh
+++ racksview_app-1.1/src/motion_detector_compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo pipx inject nuitka pyserial
-sudo nuitka --standalone --onefile motion_detector.py
+sudo ~/.local/bin/nuitka-standalone --onefile motion_detector.py
 sudo mv -f motion_detector.bin ../bin/
