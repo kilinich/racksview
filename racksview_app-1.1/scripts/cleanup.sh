27213d927dfe9f5668ba60b3c20067adb016b685
Kilinich <kilinich@gmail.com>
2025-08-25 22:49:01 +0200

    fix clenup

--- racksview_app-1.1/scripts/cleanup.sh
+++ racksview_app-1.1/scripts/cleanup.sh
@@ -6,10 +6,10 @@ TARGET_BASE="/opt/racksview/var/video"    # Base directory for recorded files
 KEEP_REC_DAYS=90
 
 # Remove old recordings
-find "${TARGET_BASE}" -type f -mtime +${KEEP_REC_DAYS} -delete
+find -L "${TARGET_BASE}" -type f -ctime +${KEEP_REC_DAYS} -delete
 
 # Remove empty directories
-find "${TARGET_BASE}" -type d -empty -delete
+find -L "${TARGET_BASE}" -type d -empty -delete
 
 /opt/racksview/scripts/stop_services.sh
 /opt/racksview/scripts/start_services.sh
