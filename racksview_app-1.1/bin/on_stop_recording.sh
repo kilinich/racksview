237d3f1c96b51c607c1236c52c5cec1cd8709879
Kilinich <kilinich@gmail.com>
2025-09-06 18:45:54 +0200

    ini and sig

--- racksview_app-1.1/bin/on_stop_recording.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Put some custom code here to notify monitoring ex. slack notification, zabbix trigger.
-echo "$(date '+%Y-%m-%d %H:%M:%S') Recording stopped... (Label: $1)" >> /var/log/racksview/recording.log
